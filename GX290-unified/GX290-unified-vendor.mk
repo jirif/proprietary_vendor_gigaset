2f5092260150164598f597eb4c72b2c5075dec7a
nift4 <nift4@protonmail.com>
2022-03-12 09:45:08 +0100

    gigaset: regenerate vendor
    Change-Id: Idc2e89d60d0fc240d932d5e63c1547b8355bc385

--- GX290-unified/GX290-unified-vendor.mk
+++ GX290-unified/GX290-unified-vendor.mk
@@ -281,8 +281,6 @@ PRODUCT_COPY_FILES += \
     vendor/gigaset/GX290-unified/proprietary/vendor/etc/mpe.conf:$(TARGET_COPY_OUT_VENDOR)/etc/mpe.conf \
     vendor/gigaset/GX290-unified/proprietary/vendor/etc/mtk_omx_core.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/mtk_omx_core.cfg \
     vendor/gigaset/GX290-unified/proprietary/vendor/etc/partition_permission.sh:$(TARGET_COPY_OUT_VENDOR)/etc/partition_permission.sh \
-    vendor/gigaset/GX290-unified/proprietary/vendor/etc/powercontable.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powercontable.xml \
-    vendor/gigaset/GX290-unified/proprietary/vendor/etc/powerscntbl.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerscntbl.xml \
     vendor/gigaset/GX290-unified/proprietary/vendor/etc/security/cacerts/00673b5b.0:$(TARGET_COPY_OUT_VENDOR)/etc/security/cacerts/00673b5b.0 \
     vendor/gigaset/GX290-unified/proprietary/vendor/etc/security/cacerts/04f60c28.0:$(TARGET_COPY_OUT_VENDOR)/etc/security/cacerts/04f60c28.0 \
     vendor/gigaset/GX290-unified/proprietary/vendor/etc/security/cacerts/0d69c7e1.0:$(TARGET_COPY_OUT_VENDOR)/etc/security/cacerts/0d69c7e1.0 \
